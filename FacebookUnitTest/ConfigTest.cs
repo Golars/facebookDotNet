3417ce2893d58f44eb0c9a1d46ff0117d5a2fb98
Golars <titorens@gmail.com>
2017-07-12 18:59:35 +0300

    init unitTest

---
+++ FacebookUnitTest/ConfigTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FacebookUnitTest
+{
+    public class ConfigTest
+    {
+        [Fact]
+        public void InitTest()
+        {
+            Assert.False(false);
+        }
+    }
+}
